29d63723a017abb5034cd46e8dc373da38029e4c
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-10 00:53:57 +0100

    add Heuristics interface

---
+++ Inducer/src/heuristic/Heuristics.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package heuristic;
+
+import datastructure.FrequentPattern;
+import java.util.List;
+
+/**
+ *
+ * @author reda
+ */
+public interface Heuristics {
+       
+    public void chooseFrequentPattern(List<FrequentPattern> input);
+    
+}
