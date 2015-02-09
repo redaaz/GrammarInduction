0cbce44bf58b07c3d47419b3ab26fdb4ec678741
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-10 00:54:17 +0100

    most frequent pattern class

---
+++ Inducer/src/heuristic/MostFrequent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package heuristic;
+
+import datastructure.FrequentPattern;
+import java.util.List;
+
+/**
+ *
+ * @author reda
+ */
+public class MostFrequent implements Heuristics {
+
+    @Override
+    public void chooseFrequentPattern(List<FrequentPattern> input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
