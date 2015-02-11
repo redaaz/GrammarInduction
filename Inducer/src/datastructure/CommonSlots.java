81a7890753c7bc2bb19a95080bb2db4f32131123
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-11 17:48:06 +0100

    add CommonSlots data structure

---
+++ Inducer/src/datastructure/CommonSlots.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datastructure;
+
+import java.util.List;
+
+/**
+ *
+ * @author reda
+ */
+public class CommonSlots {
+    public List<Integer> commonReferences;
+    public List<Integer> solts;
+    
+    
+}
