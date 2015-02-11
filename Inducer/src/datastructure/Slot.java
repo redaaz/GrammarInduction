4948a1a1cff471c7e0f6e169ccb01f8c1859b752
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-11 15:36:05 +0100

    add Slot data structure

--- Inducer/src/datastructure/Slot.java
+++ Inducer/src/datastructure/Slot.java
@@ -14,27 +14,43 @@ import java.util.List;
  * @author reda
  */
 public class Slot {
-    List<String> Alternatives;
+    List<Alternative> alternatives;
     
     public Slot(){
-        this.Alternatives=new ArrayList<>();
+        this.alternatives=new ArrayList<>();
     }
     
-    public Slot(List<String> in){
-        this.Alternatives=in;
+    public Slot(List<Alternative> in){
+        this.alternatives=in;
     }
     
-    public void addAlter(String x){
-        this.Alternatives.add(x);
+    public void addAlter(Alternative x){
+        this.alternatives.add(x);
     }
     
     public int getAlternativesCount(){
-        return this.Alternatives.size();
+        return this.alternatives.size();
     }
     
-    public List<String> getAlternatives(){
-        return this.Alternatives;
+    public List<Alternative> getAlternatives(){
+        return this.alternatives;
     }
     
+    public List<Integer> getReferenceIndexs(){
+        List<Integer> res=new ArrayList<>();
+        this.alternatives.stream().forEach(x->res.add(x.referenceIndex));
+        return res;
+    }
     
+    public void println(){
+        this.alternatives.stream().forEach(x->System.out.println(x.toString()));
+    }
+    
+    public void printlnCode(){
+        this.alternatives.stream().forEach(x->System.out.println(x.toStringCode()));
+    }
+    
+    public boolean isEmpty(){
+        return this.alternatives.isEmpty();
+    }
 }
