0694feac4bf4e453e105214d0f18c561592c4ac5
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-11 15:35:47 +0100

    add Rule data structure

--- Inducer/src/datastructure/Rule.java
+++ Inducer/src/datastructure/Rule.java
@@ -7,8 +7,9 @@
 package datastructure;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import text.General;
+
 
 /**
  *
@@ -19,12 +20,17 @@ public class Rule {
     protected static Integer idCounter=0;
     protected Integer ruleID;
     protected String leftSide;
+    protected List<Integer> referencesIndexs;
     
-    
-    public Rule(){
-        
+    public Rule(){ 
         this.ruleID=Rule.idCounter++;
         this.leftSide="<R"+this.ruleID+">";
+        WordsDictionary.addWord(leftSide);
+        referencesIndexs=new ArrayList<>();
+    }
+    
+    public void setReferencesIndexs(List<Integer> ref){
+        this.referencesIndexs=ref;
     }
     
     public String getCodedRightSide(){
@@ -42,6 +48,49 @@ public class Rule {
     @Override
     public String toString(){
         return leftSide+" -> "+getRightSide();
+    }
+    
+    public Integer getRuleIndex(){
+        return WordsDictionary.getWordIndex(leftSide);
+    }
+    
+    public void println(){
+        System.out.println(toString());
+    }
+    
+    public void printlnCoded(){
+        System.out.println(toCodedString());
+    }
+    
+    public static List<Rule> makeRules(List<Sentence> input,FrequentPattern fp){
+        List<Rule> rules=new ArrayList<>();
+        //lists of n+1 slot's alternatives (n:nb of items in the frequent pattern)
+        //each slot has a list of alternatives
+        List<Slot> slots = new ArrayList<>();
+        
+        for(int i=0;i<=fp.patterns.size();i++)
+            slots.add(new Slot());
+        
+        fp.inputReferences.stream().forEach((refIndex) -> {
+            List<List<Integer>> alters=General.split(input.get(refIndex).sentenceCode, fp.patterns);    
+            for(int i=0;i<alters.size();i++){
+                if(!alters.get(i).isEmpty()){
+                    Alternative al=new Alternative(alters.get(i),refIndex);
+                    slots.get(i).addAlter(al);
+                }
+            }
+        });
+        
+        for(Slot slot:slots){
+            if(!slot.isEmpty()){
+                SubRule sb=SubRule.makeSubRule(slot);
+                sb.println();
+                sb.printlnCoded();
+            }
+        }
+        
+        
+        return null;
     }
     
 }
