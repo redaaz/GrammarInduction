b57b9daffa58a35bbb76deabb65e334e0a470b44
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-02-19 17:25:29 +0100

    add new class

---
+++ Inducer/src/spm/spam/SPMiningAlgorithm.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spm.spam;
+
+import datastructure.FrequentPattern;
+import datastructure.Sentence;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author reda
+ */
+public class SPMiningAlgorithm {
+    
+    // Vertical database
+    public Map<Integer, Bitmap> verticalDB = new HashMap<>();
+    
+    public List<FrequentPattern> runAlgorithm2(List<String> input, double minsupRel){
+        return null;
+    }
+    
+    public List<FrequentPattern> runAlgorithm(List<Sentence> input, double minsupRel){
+        return null;
+    }
+}
