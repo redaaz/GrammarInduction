f611a3739f0a84f864b5c0c59110190fbd531255
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-04-14 16:48:30 +0200

    add converter function (CMSPADE)

--- Inducer/src/spm/spam/SPMiningAlgorithm.java
+++ Inducer/src/spm/spam/SPMiningAlgorithm.java
@@ -8,7 +8,9 @@ package spm.spam;
 
 import com.carrotsearch.hppc.IntObjectOpenHashMap;
 import datastructure.FrequentPattern;
+import datastructure.Repetition;
 import datastructure.Sentence;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,4 +39,32 @@ public class SPMiningAlgorithm {
     public String getAlgoName(){
         return "No name has inserted";
     }
+    
+    public List<FrequentPattern> stringArrayToFrequentPatternList_CMSPAM(List<String> input,List<Sentence> corpus){
+        List<FrequentPattern> res=new ArrayList<>();
+        for(String str:input){
+            ///*PERFORMANCE TEST*/long third1=System.currentTimeMillis();
+            FrequentPattern x=new FrequentPattern(str);
+            ///*PERFORMANCE TEST*/long third2=System.currentTimeMillis();
+            ///*PERFORMANCE TEST*/acc1+=third2-third1;
+            List<List<Repetition>> ref=new ArrayList<>();
+            ///*PERFORMANCE TEST*/long third3=System.currentTimeMillis();
+            x.getPattern().stream().forEach((i) -> {
+                ref.add(this.verticalDB.get(i).inputReferences);
+            });
+            ///*PERFORMANCE TEST*/long third4=System.currentTimeMillis();
+            ///*PERFORMANCE TEST*/acc2+=third4-third3;
+            x.setInputReferences(ref,corpus);
+            ///*PERFORMANCE TEST*/long third5=System.currentTimeMillis();
+            ///*PERFORMANCE TEST*/acc3+=third5-third4;
+            x.setCohesion(verticalDB);
+            ///*PERFORMANCE TEST*/long third6=System.currentTimeMillis();
+            ///*PERFORMANCE TEST*/acc4+=third6-third5;
+            res.add(x);
+        }
+        return res;
+    }
+    
+    
+    
 }
