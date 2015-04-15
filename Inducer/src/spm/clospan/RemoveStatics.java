69d0c5fb45523d9dc3ba0dc6099dc85b2cf18217
redaaz <reda.alazmeh@mailbox.tu-berlin.de>
2015-04-15 16:50:13 +0200

    add ColSpan Algorithm

---
+++ Inducer/src/spm/clospan/RemoveStatics.java
@@ -0,0 +1,16 @@
+package spm.clospan;
+
+
+
+/**
+ *
+ * @author antonio
+ */
+public class RemoveStatics {
+
+    public static void clear() {
+        ItemAbstractionPairCreator.sclear();
+        Abstraction_Qualitative.clear();
+        PatternCreator.sclear();
+    }
+}
